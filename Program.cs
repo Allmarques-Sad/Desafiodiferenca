eff0718e085999d56880f39a3947d16acd2e8032
almyr <almyr@ALLDELLOPTILEX>
2021-07-22 12:46:20 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Desafiodiferenca
+{    class Program
+    {
+        static void Main(string[] args)
+        {
+        double A = Convert.ToDouble(Console.ReadLine());
+        double B = Convert.ToDouble(Console.ReadLine());
+        double C = Convert.ToDouble(Console.ReadLine());
+        double D = Convert.ToDouble(Console.ReadLine());
+
+         Console.WriteLine("DIFERENCA = " + (A * B - C * D));
+         Console.ReadKey();
+
+        }
+    }
+}
